919b6a602f5e625d69b49a8f20ecd15f5f8a6dbf
ahmadalhaj7 <ahmad.alhaja79@gmail.com>
2026-06-09 14:44:32 +0400

    adding Swagger

--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<EventService>(); // this is for dependency injection of the event service which is used by the event controller
+builder.Services.AddEndpointsApiExplorer();// this is for endpoint explorer which is used by swagger ui
+builder.Services.AddSwaggerGen();// this is for swagger ui
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/events", (CameraEvent cameraEvent, EventService eventService) =>
+{
+    var created = eventService.Add(cameraEvent);
+    return Results.Created($"/events/{created.Id}", created);
+});
+
+app.MapGet("/events", (EventService eventService) =>
+{
+    return eventService.GetAllEvents();
+});
+
+app.MapPost("/analyze", (EventService eventService) =>
+{
+    var events = eventService.GetAllEvents();
+    return Results.Ok("Analyzing");
+});
 
 app.Run();
--- services/EventService.cs
+++ services/EventService.cs
@@ -6,7 +6,15 @@ private readonly List<CameraEvent> _events = new List<CameraEvent>();
 
 private int _nextId = 1;
 
-public List<CameraEvent> GetAllEvents()
+
+public CameraEvent Add(CameraEvent cameraEvent)
+{
+    cameraEvent.Id = _nextId++;
+    _events.Add(cameraEvent);
+    return cameraEvent;
+}
+
+public List<CameraEvent> GetAllEvents() // this method returns all the events that have been added to the service
 {
     return _events;
 
